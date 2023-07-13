48325b7559b2b462dc4c419bd93753de013c0e06
Golubytnik <rinusrenus1990@gmail.com>
2023-07-13 18:55:39 +0600

    postgres

--- Book/settings.py
+++ Book/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'Book.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'python0909',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
